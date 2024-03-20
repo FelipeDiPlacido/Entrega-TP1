4aabfed271bfbbc86c3f858969095aacc40dd366
Felipe Di Placido <diplacidofelipe@gmail.com>
2024-03-20 20:33:38 -0300

    Agregada verificación adicional para evitar la entrada de cadena vacia

--- game.py
+++ game.py
@@ -23,17 +23,26 @@ print(f"Palabra: {word_displayed}")
 for i in range(max_attempts):
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
+    
+    #Verificar si no se ingreso ninguna letra
+    if not letter:
+        print("Ingrese una letra valida")
+        continue
+    
     # Verificar si la letra ya ha sido adivinada
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
         continue
+    
     # Agregar la letra a la lista de letras adivinadas
     guessed_letters.append(letter)
+    
     # Verificar si la letra está en la palabra secreta
     if letter in secret_word:
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+    
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
@@ -43,6 +52,7 @@ for i in range(max_attempts):
             letters.append("_")
     word_displayed = "".join(letters)
     print(f"Palabra: {word_displayed}")
+    
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
